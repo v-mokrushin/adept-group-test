c0244f4c3115c2077d33ab9c2fdfdd8b408877a1
Vadim Mokrushin <mokr.vo@yandex.ru>
2024-07-15 21:05:34 +0700

    Создание проекта, установка библиотек, залож. основа FSD

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import logo from "./logo.svg";
+import styles from "./index.module.scss";
+
+export const App: React.FC = () => {
+  return <div className={styles.root}>App</div>;
+};
